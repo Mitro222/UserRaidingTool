affca35b4131a35eebb344303fdc5610ec1fccf6
mrMiiao <90341146+mrMiiao@users.noreply.github.com>
2021-12-09 16:49:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,12 @@ from config import token, msg
 
 client = User(token=token)
 
-@user.on.chat_message()
+a = 1
+
+
+@client.on.chat_message()
 async def raid(event: Message);
-  a = 1
-  while a > 0:
-    await event.answer(msg)
+    while a > 0:
+        await event.answer(msg)
 
 client.run_forever()
